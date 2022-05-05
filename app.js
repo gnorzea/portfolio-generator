1082c133a5b6571cd3e1c7dd99f0afcdcadecda7
Thunnder <gnorzea@gmail.com>
2022-05-05 10:04:32 -0400

    added inquirer

--- app.js
+++ app.js
@@ -11,19 +11,27 @@
 //   };
 
 //   printProfileData(profileDataArgs);
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
 
 
+const inquirer = require('inquirer');
+// const fs = require('fs');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const generatePage = require('./src/page-template.js');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
