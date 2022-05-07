1b9a31d25548fd469ed1390da2514e56f346d0a6
Thunnder <gnorzea@gmail.com>
2022-05-07 13:17:54 -0400

    finish portfolio HTML output

--- app.js
+++ app.js
@@ -14,17 +14,9 @@
 
 
 const inquirer = require('inquirer');
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -76,15 +68,18 @@ const promptUser = () => {
 };
 
 const promptProject = portfolioData => {
-  // If there's no 'projects' array property, create one
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
+ 
   console.log(`
 =================
 Add a New Project
 =================
 `);
+
+ // If there's no 'projects' array property, create one
+ if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+
   return inquirer.prompt([
     {
       type: 'input',
@@ -157,5 +152,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+   const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('index.html', pageHTML, err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
   });
